72ab19cf4e702d59d5cdabeaecfac2271bfbaa14
rasulkimsanbaevich-arch <rasulkimsanbaevich@gmail.com>
2025-10-30 14:25:02 +0300

    howe work

--- home_work1.py
+++ home_work1.py
@@ -1,2 +1,19 @@
+class FootballPlayer:
+    def __init__(self, name, position, rating):
+        self.name = name
+        self.position = position
+        self.rating = rating
+
+    def action(self):
+        return (f"Hi my name is {self.name} i'm {self.position}. My rating is {self.rating}")
+    def prime_rating(self):
+        self.rating += 6
+        return (f"{self.name}'s prime rating was {self.rating}")
 
 
+messi = FootballPlayer("Messi", "Forward", 88)
+ronaldo = FootballPlayer("Ronaldo", "Forward", 85)
+
+print(messi.action())
+print(ronaldo.action())
+print(messi.prime_rating())
